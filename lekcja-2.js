286fe127968ed42970f30ac6c9670c61f6f1e9fa
maciej.bartynski <maciej.bartynski@amsterdamstandard.com>
2022-11-18 16:29:40 +0100

    polecenie

--- lekcja-2.js
+++ lekcja-2.js
@@ -6,8 +6,9 @@
  * 
  * lekcja-2.logicalComparators.js.
  * 
- * Wykonaj je, czytając plik z góry na dół. Zazwyczaj musisz przeczytać fragment, odkomentować kod tylko tuż pod nim,
- * wykonać lub zaobserwować coś na tym kodzie, po czym zakomentować ten kod i przejsć do następnego
+ * Wykonaj je, czytając plik z góry na dół. Zazwyczaj musisz przeczytać fragment, odkomentować kod pod nim
+ * (wszystkie linijki do następnego fragmentu zakomentowanego tekstu),
+ * wykonać lub zaobserwować coś na tym kodzie, po czym znów zakomentować ten kod i przejsć do następnego
  * fragmentu tekstu. Poradzisz sobie!
  * 
  */

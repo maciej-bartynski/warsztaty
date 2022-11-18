af450390fb59517aa0b402229807d7661bd1d265
maciej.bartynski <maciej.bartynski@amsterdamstandard.com>
2022-11-18 16:26:25 +0100

    lekcja-2

---
+++ lekcja-2.js
@@ -0,0 +1,13 @@
+
+/**
+ * Hello!
+ * 
+ * Na razie zadania są tylko w pliku 
+ * 
+ * lekcja-2.logicalComparators.js.
+ * 
+ * Wykonaj je, czytając plik z góry na dół. Zazwyczaj musisz przeczytać fragment, odkomentować kod tylko tuż pod nim,
+ * wykonać lub zaobserwować coś na tym kodzie, po czym zakomentować ten kod i przejsć do następnego
+ * fragmentu tekstu. Poradzisz sobie!
+ * 
+ */
